7da273927e8f1a72c1bd9026c9432c99adaa48e3
gl_iliauchuk <Ihar.Liauchuk@godel.shellenergy.co.uk>
2024-10-10 17:11:10 +0200

    Mod: removed unnecessary array

--- usecases/fetchAsteroid.js
+++ usecases/fetchAsteroid.js
@@ -11,21 +11,20 @@ async function fetchAsteroid(date, showDangerous, showCount) {
 function transformBody(jsonBody, showCount, showDangerous) {
     const result = {};
     let wereDangerousMeteors = false;
-    const asteroids = [];
 
-    Object.values(jsonBody.near_earth_objects).flatMap(d => d)
-        .forEach(asteroid => {
+    const asteroids = Object.values(jsonBody.near_earth_objects).flatMap(d => d)
+        .map(asteroid => {
             if (!wereDangerousMeteors && asteroid.is_potentially_hazardous_asteroid) {
                 wereDangerousMeteors = true;
             }
-            asteroids.push({
-                "id" : asteroid.id,
-                "name" : asteroid.name,
-                "diameter" : asteroid.estimated_diameter.meters,
-                "is_potentially_hazardous_asteroid" : asteroid.is_potentially_hazardous_asteroid,
-                "close_approach_date_full" : asteroid.close_approach_data[0].close_approach_date_full,
-                "relative_velocity" : asteroid.close_approach_data[0].relative_velocity.kilometers_per_second
-            });
+            return {
+                "id": asteroid.id,
+                "name": asteroid.name,
+                "diameter": asteroid.estimated_diameter.meters,
+                "is_potentially_hazardous_asteroid": asteroid.is_potentially_hazardous_asteroid,
+                "close_approach_date_full": asteroid.close_approach_data[0].close_approach_date_full,
+                "relative_velocity": asteroid.close_approach_data[0].relative_velocity.kilometers_per_second
+            };
         });
     
     if (showCount) {
